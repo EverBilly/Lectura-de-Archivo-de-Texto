057a32c67cc6eb081b5316dd53e0513edb191d1a
Ever Cifuentes <everbilly35@gmail.com>
2018-07-20 10:58:08 -0600

    [ADDED] add new repository of socket

---
+++ socket/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace socket
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConexion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConexion
+            // 
+            this.btnConexion.Cursor = System.Windows.Forms.Cursors.NoMove2D;
+            this.btnConexion.Location = new System.Drawing.Point(295, 187);
+            this.btnConexion.Name = "btnConexion";
+            this.btnConexion.Size = new System.Drawing.Size(146, 38);
+            this.btnConexion.TabIndex = 0;
+            this.btnConexion.Text = "Conexion";
+            this.btnConexion.UseVisualStyleBackColor = true;
+            this.btnConexion.Click += new System.EventHandler(this.btnConexion_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 286);
+            this.Controls.Add(this.btnConexion);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConexion;
+    }
+}
+
---
+++ socket/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows;
+
+namespace socket
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConexion_Click(object sender, EventArgs e)
+        {
+            try
+            {   // Abrir el Archivo Utilizando StreamReader.
+                using (StreamReader sr = new StreamReader("C:\\ArchivoPrueba.txt"))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    String line = sr.ReadToEnd();
+                    MessageBox.Show("Archivo Leido: " + Environment.NewLine + line);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("El Archivo No Pudo Ser Leido");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
