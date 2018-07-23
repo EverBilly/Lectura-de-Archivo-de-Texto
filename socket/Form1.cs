ff354c1f6becddff9b3e328b66807a3dad77e7cb
Ever Cifuentes <everbilly35@gmail.com>
2018-07-23 15:31:52 -0600

    [UPDATED] added function for select the file and save the file in xml whi the data of the file txt

--- socket/Form1.cs
+++ socket/Form1.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Windows;
+using System.Configuration;
+using System.Xml.Linq;
+using System.Xml;
 
 namespace socket
 {
@@ -32,7 +35,7 @@ namespace socket
                 {
                     // Leer el Archivo y Mostrar la Informacion del Documento.
                     String linea = sr.ReadToEnd();
-                    MessageBox.Show("Archivo Leido: " + Environment.NewLine + linea);
+                    textBox4.Text = ("Archivo Leido: " + Environment.NewLine + linea);
                 }
             }
             catch (Exception ex)
@@ -54,7 +57,86 @@ namespace socket
             {
                 this.textBox1.Text = abrirarchivo.FileName;
             }
+            //XML();
         }
 
+        public class AppConfig
+        {
+            public static void EstableceParametros(string clave, string palabra)
+            {
+                Configuration conf = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+                conf.AppSettings.Settings.Remove(clave);
+
+                conf.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection("appSetings");
+                conf.AppSettings.Settings.Add(clave, palabra);
+                conf.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection("appSetings");
+
+            }
+
+            public static string RecuperaValor(string clave, string predeterminado)
+            {
+                string retornar = ConfigurationManager.AppSettings[clave];
+                if (retornar == null)
+                {
+                    retornar = predeterminado;
+                }
+                return retornar;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AppConfig.EstableceParametros(Convert.ToString(textBox2.Text).Trim(), Convert.ToString(textBox3.Text).Trim());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = AppConfig.RecuperaValor(textBox2.Text, "Error");
+        }
+
+       //CARGAR ARCHIVO XML
+        public void XML()
+        {
+            XElement root = new XElement(Convert.ToString(textBox2.Text).Trim());
+            foreach (string line in File.ReadAllLines(textBox1.Text)) {
+                string[] fields = line.Split('|');
+                XElement record = new XElement(Convert.ToString(textBox3.Text).Trim());
+                int pos = 0;
+                foreach (String sp in fields)
+                {
+                    pos += 1;
+                    XElement field = new XElement(string.Format("posicion", pos.ToString()), new XAttribute("att", "content"), new XAttribute("name", "SAT")); // prepare child nodes
+                    field.Add(sp);
+                    record.Add(field); // add to parent node
+                }
+                root.Add(record);
+            }
+            textBox5.Text = (root.ToString());  // display the result on console
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog abrirarchivo = new OpenFileDialog();
+            abrirarchivo.Title = "Seleccione El Archivo de Texto";
+            abrirarchivo.Filter = "Documentos de Texto (*.txt)|*.txt";
+            abrirarchivo.FileName = textBox1.Text;
+
+            if (abrirarchivo.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.textBox1.Text = abrirarchivo.FileName;
+            }
+            XML();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            XmlDocument documento = new XmlDocument();
+            documento.LoadXml(textBox5.Text);
+            documento.PreserveWhitespace = true;
+            documento.Save("data.xml");
+        }
     }
 }
